f0ee5f3d28bfecc4757a9017a449021290ff6ba4
Francesco Alemanno <francescoalemanno710@gmail.com>
2019-05-30 09:12:11 +0200

    Fixed corner case bug and corrected indentation
    if the integral was performed over a 0-vector array the result was a 
    0-vector array, now it is correctly a scalar = 0

--- src/Trapz.jl
+++ src/Trapz.jl
@@ -1,62 +1,62 @@
 module Trapz
-export trapz
+    export trapz
 
 
-@inline function bringlast(T::Tuple,el)
-    ifelse(el==T[1],
-        (bringlast(Base.tail(T),el)...,T[1]) ,
-        (T[1],bringlast(Base.tail(T),el)...)
-    )
-end
-@inline function bringlast(T::Tuple{},el); T; end
-
-@inline function trapz_colon(k) Colon(); end
-@inline function idxlast(i,::Val{N}) where N; Base.tail((ntuple(trapz_colon,Val(N))...,i)) end
-
-
-function trapz(x::T1, y::T2) where {N,fT,T1<:AbstractVector{fT},T2<:AbstractArray{fT,N}}
-    n = length(x)
-    s = size(y)
-    @assert s[end]==n
-    @inbounds begin
-        r =  zeros(fT,Base.reverse(Base.tail(Base.reverse(s))))
-        if n == 1; return r; end
-        for i in 2:n-1
-           @fastmath r .+= (x[i+1] - x[i-1]) .* view(y,idxlast(i,Val(N))...)
-        end
-        r .+= (x[end]-x[end-1]) .* view(y,idxlast(n,Val(N))...) + (x[2] - x[1]).* view(y,idxlast(1,Val(N))...)
-        return r./2
+    @inline function bringlast(T::Tuple,el)
+        ifelse(el==T[1],
+            (bringlast(Base.tail(T),el)...,T[1]) ,
+            (T[1],bringlast(Base.tail(T),el)...)
+        )
     end
-end
+    @inline function bringlast(T::Tuple{},el); T; end
 
-function trapz(x::T1, y::T2, axis::T3) where {N,fT,T1<:AbstractVector{fT},T2<:AbstractArray{fT,N},T3<:Integer}
-    @assert 1<=axis<=N
-    trapz(x,PermutedDimsArray(y,bringlast(ntuple(identity,Val(N)),axis)))
-end
+    @inline function trapz_colon(k) Colon(); end
+    @inline function idxlast(i,::Val{N}) where N; Base.tail((ntuple(trapz_colon,Val(N))...,i)) end
 
 
-"""
-    trapz(x,y,axis=End)
-    Calculates ∫y[..., i (axis) ,...] dx[i]
-    for performance reasons x must have the same float type as y.
-    1-D Example:
-```julia
-    vx=0:0.01:1
-    vy=(x->x^2).(vx)
-    trapz(vx,vy)
-```
-        Result ≈ 1/3
+    function trapz(x::T1, y::T2) where {N,fT,T1<:AbstractVector{fT},T2<:AbstractArray{fT,N}}
+        n = length(x)
+        s = size(y)
+        @assert s[end]==n
+        @inbounds begin
+            r =  zeros(fT,Base.reverse(Base.tail(Base.reverse(s))))
+            if n == 1; return r.*zero(fT); end
+            for i in 2:n-1
+               @fastmath r .+= (x[i+1] - x[i-1]) .* view(y,idxlast(i,Val(N))...)
+            end
+            r .+= (x[end]-x[end-1]) .* view(y,idxlast(n,Val(N))...) + (x[2] - x[1]).* view(y,idxlast(1,Val(N))...)
+            return r./2
+        end
+    end
 
-    2-D Example:
-```julia
-    vx=0:0.01:1
-    vy=0:0.01:2
-    z=[x^2+y^2 for x=vx, y=vy]
-    trapz(trapz(vy,z),vx)
-```
-        Result ≈ 4/3
-"""
-trapz
+    function trapz(x::T1, y::T2, axis::T3) where {N,fT,T1<:AbstractVector{fT},T2<:AbstractArray{fT,N},T3<:Integer}
+        @assert 1<=axis<=N
+        trapz(x,PermutedDimsArray(y,bringlast(ntuple(identity,Val(N)),axis)))
+    end
+
+
+    """
+        trapz(x,y,axis=End)
+        Calculates ∫y[..., i (axis) ,...] dx[i]
+        for performance reasons x must have the same float type as y.
+        1-D Example:
+    ```julia
+        vx=0:0.01:1
+        vy=(x->x^2).(vx)
+        trapz(vx,vy)
+    ```
+            Result ≈ 1/3
+
+        2-D Example:
+    ```julia
+        vx=0:0.01:1
+        vy=0:0.01:2
+        z=[x^2+y^2 for x=vx, y=vy]
+        trapz(trapz(vy,z),vx)
+    ```
+            Result ≈ 4/3
+    """
+    trapz
 
 
 end # module
