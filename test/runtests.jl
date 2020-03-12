8acfc616678295a97bab0e073d5134ab2ff95cd3
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-03-12 20:58:41 +0100

    faster test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,10 @@
 using Test
 using Trapz
-
+@info "Started Package Testing"
 @testset "Basic Tests" begin
-    vx=range(0,1,length=100)
-    vy=range(0,2,length=200)
-    vz=range(0,3,length=300)
+    vx=range(0,1,length=5)
+    vy=range(0,2,length=5)
+    vz=range(0,3,length=5)
     M=[x^2+y^2+z^2 for x=vx,y=vy,z=vz]
-    @test trapz((vx,vy,vz), M) ≈ 28.000303707970264
+    @test trapz((vx,vy,vz), M) == 28.875
 end
