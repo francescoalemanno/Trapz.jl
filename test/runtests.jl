bebf55f382c6f502af814b3335459bba701333ca
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-03-12 21:09:09 +0100

    test 1 whole permutation

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,11 @@ using Trapz
 @info "Started Package Testing"
 @testset "Basic Tests" begin
     vx=range(0,1,length=5)
-    vy=range(0,2,length=5)
-    vz=range(0,3,length=5)
+    vy=range(0,2,length=10)
+    vz=range(0,3,length=15)
     M=[x^2+y^2+z^2 for x=vx,y=vy,z=vz]
-    @test trapz((vx,vy,vz), M) == 28.875
+    res=28.157801083396322
+    @test trapz((vx,vy,vz), M) ≈ res
+    @test trapz((vz,vy,vx), M, (3,2,1)) ≈ res
+    @test trapz((vy,vx,vz), M, (2,1,3)) ≈ res
 end
